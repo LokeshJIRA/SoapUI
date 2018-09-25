e298f46e1eb534fab625f20f4cd217831be98165
Lokesh <lokeshachary353@gmail.com>
2018-09-26 01:03:27 +0530

    commit

---
+++ src/Features/Success.feature
@@ -0,0 +1,7 @@
+Feature: Validate the Sucessfull response 200 ok
+
+Scenario: Validate the Request and Response scenarios with REST Client
+
+Given: URL send the reuest for Hyderabad 
+Then: server will send the successful response
+When: Client got the successfull response from the server scenario pass
--- src/Runner/testRunner.java
+++ src/Runner/testRunner.java
@@ -1,11 +1,18 @@
 package Runner;
 
+import org.junit.runner.RunWith;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.junit.runner.RunWith;
 
-//@RunWith(Cucumber.class)
+import cucumber.api.CucumberOptions;
+import cucumber.api.java.it.Date;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
 @CucumberOptions(
-         features="Features",
+         features="C:\\Users\\user\\eclipse-workspace\\WebService_REST\\src\\Features\\Success.feature",
          glue= {"StepDefination"} )
 public class testRunner extends AbstractTestNGCucumberTests {
 
